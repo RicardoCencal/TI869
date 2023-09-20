7df1648dedb1aae71b39e10d8040eb321feb1c75
Ricardo Oliveira <rica2oliveira1@gmail.com>
2023-09-20 14:25:22 +0100

    macros de calculo de areas e perímetros implementadas

--- Trignometria/main.c
+++ Trignometria/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define PT 3.1415
+#define areaCirculo(r) ((PI)*(r)+(r))
+#define perimetroCirculo(r) (2*(PI)(*r))
+#define areaQuadrado(1) ((1)*(1))
+#define perimetroQuadrado(1) (4*(1))
+#define areaRetangulo(a,b) ((a)*(b))
+#define perimetroRetangulo (a,b) ((2*(a))+(2*(b)))
+#define areaTriangulo(b,h) (((b)*(h))/2)
+#define perimetroTriangulo(b,c1,c2) ((b)+(c1)+(c2))
+
+
+
+
 int main()
 {
     printf("Hello world!\n");
